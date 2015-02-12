d9186f32a00effe4e349cca0dab61113e32075ca
Robert M Flight <r.flight79@gmail.com>
2015-02-12 09:45:09 -0500

    getting the package environment to work on

---
+++ R/class_graph.R
@@ -0,0 +1,33 @@
+#' class graph of package
+#' 
+#' @param package the directory or package name in current environment
+#' @param depth how deep to go for classes \emph{outside} the package (default = 0)
+#' 
+#' @import devtools
+#' @export
+#' @return graph of classes
+class_graph <- function(package = ".", depth = 0){
+  package_env <- get_package_env(package)
+}
+
+#' get package env
+#' 
+#' @param package string giving the package environment to find
+#' 
+#' @return environment
+#' @export
+get_package_env <- function(package = "."){
+  stopifnot(is.character(package))
+  
+  has_package <- grepl("^package:", package)
+  
+  if ((package = ".") || (!has_package)){
+    package_env <- devtools::load_all(devtools::as.package(package))$env
+  } else {
+      loaded_packages <- search()
+      pos <- match(package, loaded_packages)
+      package_env <- as.environment(pos)
+    }
+  package_env
+  
+}
